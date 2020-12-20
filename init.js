f1af15764d4921a6d954fb81a4f084458caa0b9b
Chan-hyeong Lee <dannychh@khu.ac.kr>
2020-12-20 18:22:52 +0900

    Fix init.js

--- init.js
+++ init.js
@@ -1,9 +1,9 @@
 'use strict';
 
-import app from 'app';
+import app from './app';
 
 const PORT = 4000;
 
-const handleListening = () => console.log(`✅Listening on: http://localhost:${PORT}`);
+const handleListening = () => console.log(`✅ Listening on: http://localhost:${PORT}`);
 
 app.listen(PORT, handleListening);
