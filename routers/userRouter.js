c4f717d20faaf5ea6dbff181038b4345da6a7cbc
Chan-hyeong Lee <dannychh@khu.ac.kr>
2020-12-21 12:39:07 +0900

    Fix userRouter.js and routes.js

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -7,8 +7,8 @@ import { changePassword, editProfile, userDetail, users } from '../controllers/u
 const userRouter = express.Router();
 
 userRouter.get("/", users);
-userRouter.get(routes.userDetail, userDetail);
 userRouter.get(routes.editProfile, editProfile);
 userRouter.get(routes.changePassword, changePassword);
+userRouter.get(routes.userDetail, userDetail);
 
 export default userRouter;
