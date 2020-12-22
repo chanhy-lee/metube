04de8626b696cdf359fa01ae18cc0d6376a4aa96
Chan-hyeong Lee <dannychh@khu.ac.kr>
2020-12-22 16:56:02 +0900

    Update userRouter.js, videoRouter.js, and controllers/*

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -2,11 +2,10 @@
 
 import express from 'express';
 import routes from '../routes';
-import { changePassword, editProfile, userDetail, users } from '../controllers/userController';
+import { changePassword, editProfile, userDetail } from '../controllers/userController';
 
 const userRouter = express.Router();
 
-userRouter.get("/", users);
 userRouter.get(routes.editProfile, editProfile);
 userRouter.get(routes.changePassword, changePassword);
 userRouter.get(routes.userDetail, userDetail);
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -2,11 +2,10 @@
 
 import express from 'express';
 import routes from '../routes';
-import { deleteVideo, editVideo, upload, videoDetail, videos } from '../controllers/videoController';
+import { deleteVideo, editVideo, upload, videoDetail } from '../controllers/videoController';
 
 const videoRouter = express.Router();
 
-videoRouter.get("/", videos);
 videoRouter.get(routes.upload, upload);
 videoRouter.get(routes.videoDetail, videoDetail);
 videoRouter.get(routes.editVideo, editVideo);
