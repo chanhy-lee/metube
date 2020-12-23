37d6c084189f1b6368377db77c2566644c3e3140
Chan-hyeong Lee <dannychh@khu.ac.kr>
2020-12-23 11:16:16 +0900

    Update middlewares.js

--- middlewares.js
+++ middlewares.js
@@ -5,5 +5,5 @@ import routes from "./routes";
 export const localsMiddleware = (req, res, next) => {
     res.locals.siteName = 'MeTube';
     res.locals.routes = routes;
-    next(); // for route functions
+    next(); // to route functions
 };
