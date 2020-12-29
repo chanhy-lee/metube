665f06fc075646d173455bb62963190dbb7e5e4a
ChanHyLee <dannychh@khu.ac.kr>
2020-12-29 23:39:01 +0900

    Add Comment.js, and update init.js and Video.js

--- models/Video.js
+++ models/Video.js
@@ -19,7 +19,13 @@ const VideoSchema = new mongoose.Schema({
     createdAt: {
         type: Date,
         default: Date.now
-    }
+    },
+    comments: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Comment"
+        }
+    ]
 });
 
 const model = mongoose.model('Video', VideoSchema);
