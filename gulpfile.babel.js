874a356945044722e877e6a06384b8fd1f64e473
ChanHyLee <dannychh@khu.ac.kr>
2021-01-09 12:41:14 +0900

    Update package.json, gulpfile.babel.js, and .gitignore, and add src/

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,1 +1,35 @@
+'use strict';
+
+import del from 'del';
 import gulp from 'gulp';
+import sass from 'gulp-sass';
+
+sass.compiler = require('node-sass');
+
+const gRoutes = {
+    scss: {
+        watch: "src/scss/**/*.scss",
+        src: "src/scss/style.scss",
+        dest: "build/css"
+    }
+};
+
+const styles = () =>
+    gulp
+        .src(gRoutes.scss.src)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest(gRoutes.scss.dest));
+
+const clean = () => del(['build/']);
+
+const watch = () => {
+    gulp.watch(gRoutes.scss.watch, styles);
+};
+
+const prepare = gulp.series([clean]);
+
+const assets = gulp.series([styles]);
+
+const postDev = gulp.series([watch]);
+
+export const dev = gulp.series([prepare, assets, postDev]);
